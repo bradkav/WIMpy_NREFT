c3d6cdf7287e565ccf8e8a109e5cbb1180d60490
bradkav <bradley.kavanagh@cantab.net>
2018-03-28 17:47:58 +0200

    Added Wfunctions_test.ipynb for checking python implementation of Wfunctions

--- Wfunctions/Makefile.sh
+++ Wfunctions/Makefile.sh
@@ -1,10 +1,9 @@
 objects = WM.so WMP2.so WP1.so WP2.so WS1.so WS2.so WS1D.so WD.so
 
-
 all: $(objects)
 
 clean:
 	rm $(objects)
 
 %.so: %.f
-	python2 -m numpy.f2py -c $^ -m $(basename $@) 
+	python2 -m numpy.f2py -c $^ -m $(basename $@)
