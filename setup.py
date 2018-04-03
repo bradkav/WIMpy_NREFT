ea1890f22629bf5bfe2967e91101fd8a395e787b
tedwards2412 <tedwards2412@gmail.com>
2018-04-03 15:03:15 +0100

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='swordfish',
+    version='1.0.0',
+    description='Calculates the DM direct detection signal according to the NREFT',
+    author='Bradley Kavanagh',
+    author_email='b.j.kavanagh@uva.nl',
+    url = 'https://github.com/bradkav/WIMpy_NREFT',
+    packages=['WIMpy'],
+    package_data={'WIMpy': [] },
+    long_description=open('README.md').read(),
+)
